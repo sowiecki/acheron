29c4a4a4b3ab9c988f9b716eb00c5eaa2b4c6ff4
Sean <me@seanowiecki.com>
2020-03-15 15:15:03 -0500

    CORS fix.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,32 +1,36 @@
 /* eslint new-cap:0 */
-import express from "express";
+import express from 'express';
 
-import eventsController from "../controllers/events";
-import forwardController from "../controllers/forward";
-import applicationView from "../views/application";
-import { AUTHORIZED_HEADERS, AUTHORIZED_METHODS } from "../constants";
+import eventsController from '../controllers/events';
+import forwardController from '../controllers/forward';
+import applicationView from '../views/application';
+import { AUTHORIZED_HEADERS, AUTHORIZED_METHODS } from '../constants';
 
 const router = express.Router();
 
 router.use((req, res, next) => {
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
-  res.header("Access-Control-Allow-Methods", AUTHORIZED_METHODS);
-  res.header("Access-Control-Allow-Headers", AUTHORIZED_HEADERS);
-  next();
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', AUTHORIZED_METHODS);
+  res.header('Access-Control-Allow-Headers', AUTHORIZED_HEADERS);
+  if ('OPTIONS' == req.method) {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
 });
 
-router.post("/api/forward", (req, res) => forwardController.handle(req, res));
+router.post('/api/forward', (req, res) => forwardController.handle(req, res));
 
 /* Room pings */
-router.post("/api/:event", (req, res) => eventsController.handle(req, res));
+router.post('/api/:event', (req, res) => eventsController.handle(req, res));
 
 /* AWS Healthcheck */
-router.get("/ping", (req, res) => {
-  res.status(200).send("pong");
+router.get('/ping', (req, res) => {
+  res.status(200).send('pong');
 });
 
 /* Serve client - must be last route */
-router.get("*", (req, res) => res.send(applicationView));
+router.get('*', (req, res) => res.send(applicationView));
 
 export default router;
