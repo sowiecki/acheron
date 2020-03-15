df886ebb18a3735cee19f35e043c2b5032e72a6d
Sean <me@seanowiecki.com>
2020-03-15 14:46:18 -0500

    Remove selfsigned cert.

--- server/server.js
+++ server/server.js
@@ -1,37 +1,38 @@
 /* eslint no-console:0 */
 /* globals console */
-import https from 'https';
-import fs from 'fs';
-import path from 'path';
-import cors from 'cors';
-import express from 'express';
-import favicon from 'serve-favicon';
-import logger from 'morgan';
-import cookieParser from 'cookie-parser';
-import bodyParser from 'body-parser';
+import https from "https";
+// import fs from 'fs';
+// import path from 'path';
+import cors from "cors";
+import express from "express";
+import favicon from "serve-favicon";
+import logger from "morgan";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
-import { SERVER_PORT, PUBLIC_PATH, VIEWS_PATH } from './config';
-import socketController from './controllers/socket';
-import routes from './routes';
+import { SERVER_PORT, PUBLIC_PATH, VIEWS_PATH } from "./config";
+import socketController from "./controllers/socket";
+import routes from "./routes";
 
 const app = express();
-const key = fs.readFileSync(path.resolve(__dirname, '../key.pem'));
-const cert = fs.readFileSync(path.resolve(__dirname, '../cert.pem'));
+// const key = fs.readFileSync(path.resolve(__dirname, '../key.pem'));
+// const cert = fs.readFileSync(path.resolve(__dirname, '../cert.pem'));
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(favicon(`${PUBLIC_PATH}/favicon.ico`));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.set('port', SERVER_PORT);
-app.set('views', VIEWS_PATH);
-app.use('/', express.static(PUBLIC_PATH));
-app.use('/', routes);
+app.set("port", SERVER_PORT);
+app.set("views", VIEWS_PATH);
+app.use("/", express.static(PUBLIC_PATH));
+app.use("/", routes);
 
 // const server = http.createServer(app);
-const server = https.createServer({key: key, cert: cert }, app);
+const server = https.createServer(app);
+// const server = https.createServer({key: key, cert: cert }, app);
 
-server.listen(SERVER_PORT, (err) => {
+server.listen(SERVER_PORT, err => {
   if (err) {
     console.log(err);
     return;
